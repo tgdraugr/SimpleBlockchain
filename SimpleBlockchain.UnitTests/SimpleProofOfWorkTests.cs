c3c328943696ea501d19127ca10cb0d4db7db972
tgdraugr <tiagogabrielsilva0@gmail.com>
2022-05-27 15:56:33 +0100

    Add case for deterministic hash function for testing

--- SimpleBlockchain.UnitTests/SimpleProofOfWorkTests.cs
+++ SimpleBlockchain.UnitTests/SimpleProofOfWorkTests.cs
@@ -14,18 +14,52 @@ public class SimpleProofOfWorkTests
         "PreviousHash");
     
     [Fact]
-    public void Should_brew_a_valid_nonce_for_mining_block()
+    public void Should_brew_a_nonce_when_hash_has_predefined_count_of_leading_zeros()
     {
-        var pow = new SimpleProofOfWork();
+        var expectedHashesPerNonce = new Dictionary<int, string>
+        {
+            { 0, "abc" },
+            { 1, "x91" },
+            { 2, "ax1" },
+            { 3, "0x1" },
+            { 4, "00a" },
+            { 5, "0a2" }
+        };
+
+        var hashProducer = new AnotherHashProducer(expectedHashesPerNonce);
+        var pow = new SimpleProofOfWork(hashProducer);
+        
         var nonce = pow.NewNonce(DummyBlock);
-        Assert.Equal(1, nonce);
+        
+        Assert.Equal(4, nonce);
+    }
+}
+
+public class AnotherHashProducer : IProduceHash
+{
+    private int _index;
+    private readonly Dictionary<int, string> _hashesPerNonce;
+
+    public AnotherHashProducer(Dictionary<int,string> hashesPerNonce)
+    {
+        _hashesPerNonce = hashesPerNonce;
+    }
+
+    public string GeneratedHash(string input)
+    {
+        return _hashesPerNonce[_index++];
     }
 }
 
 public class SimpleProofOfWork : IBrewNonce
 {
-    private bool proofState = true;
-    
+    private readonly AnotherHashProducer _hashProducer;
+
+    public SimpleProofOfWork(AnotherHashProducer hashProducer)
+    {
+        _hashProducer = hashProducer;
+    }
+
     public int NewNonce(Block lastMinedBlock)
     {
         var nonce = 0;
@@ -37,6 +71,7 @@ public class SimpleProofOfWork : IBrewNonce
 
     private bool IsValidProof(Block lastMinedBlock, int nonce)
     {
-        return proofState = !proofState;
+        var hash = _hashProducer.GeneratedHash("");
+        return hash.StartsWith("00");
     }
 }
