ee28d775a76683aa9764f68aa5d4265f6ac3fd3a
tgdraugr <tiagogabrielsilva0@gmail.com>
2022-05-29 15:43:20 +0100

    Add test case for fetching the full blockchain

---
+++ SimpleBlockchain.IntegrationTests/BlockchainTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using SimpleBlockchain.API;
+using Xunit;
+
+namespace SimpleBlockchain.IntegrationTests;
+
+public class BlockchainTests : IntegrationTest
+{
+    public BlockchainTests(GrpcTestFixture<Startup> fixture) : base(fixture)
+    {
+    }
+
+    [Fact]
+    public async Task Should_get_the_full_blockchain_containing_only_the_genesis_block()
+    {
+        var client = new BlockchainNode.BlockchainNodeClient(Channel);
+        var reply = await client.GetChainAsync(new Empty());
+        Assert.Equal(1, reply.Length);
+    }
+}
