4c379fa7f51e3cf3846132738976c32c1c0bc44d
tgdraugr <tiagogabrielsilva0@gmail.com>
2022-05-29 15:20:46 +0100

    Add comment about the context of the test

--- SimpleBlockchain.IntegrationTests/GreeterTests.cs
+++ SimpleBlockchain.IntegrationTests/GreeterTests.cs
@@ -13,6 +13,7 @@ public class GreeterTests : IntegrationTest
     [Fact]
     public async Task Should_say_hello_with_given_name()
     {
+        // this will work as a base test for guaranteeing the server is in the right state
         var client = new Greeter.GreeterClient(Channel);
         var response = await client.SayHelloAsync(new HelloRequest { Name = "Someone" });
         Assert.Equal("Hello Someone", response.Message);
