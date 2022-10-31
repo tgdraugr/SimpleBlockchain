0a5c56cd435e85a587859adcc9b71c8253ea448b
tgdraugr <tiagogabrielsilva0@gmail.com>
2022-10-31 23:54:21 +0000

    chore: clean up on non needed file

--- SimpleBlockchain.IntegrationTests/BlockchainTests.cs
+++ SimpleBlockchain.IntegrationTests/BlockchainTests.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Google.Protobuf.WellKnownTypes;
 using Microsoft.Extensions.DependencyInjection;
 using SimpleBlockchain.API;
